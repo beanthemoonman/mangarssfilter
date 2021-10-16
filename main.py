5479fc8bb0fbf0593dccaefcfe8bb085b06630aa
Alexander Bean Apmann <caboose200@gmail.com>
2021-10-16 14:35:47 -0400

    refactor

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+#!/usr/bin/python
 import requests
 import rfeed
 import feedparser
+import datetime
+import argparse
 from lxml import html, etree
 
 
-rssfeed = 'https://www.mangaupdates.com/rss.php'
-linksfeed = 'https://www.mangaupdates.com/mylist.html?id=598460&list=read'
+def getFollowedSeries(linksfeed):
+    linkspage = requests.get(linksfeed)
+    linkshtml = html.fromstring(linkspage.content)
+    return linkshtml.xpath('//a[@title="Series Info"]/@href')
+
+def curateRssFeed(linksfeed,rssfeed):
+    linkstable = getFollowedSeries(linksfeed)
+    rss = feedparser.parse(rssfeed)
+    for i in rss.entries:
+        if hasattr(i,'link') and i.link in linkstable:
+            yield rfeed.Item(
+                title=i.title,
+                link=i.link,
+                description=i.description
+            )
 
 if __name__ == "__main__":
-    linkspage = requests.get(linksfeed)
+    rssfeed = 'https://www.mangaupdates.com/rss.php'
+    linksfeed = 'https://www.mangaupdates.com/mylist.html?id=598460&list=read'
 
-    linkshtml = html.fromstring(linkspage.content)
-
-    linkstable = linkshtml.xpath('//a[@title="Series Info"]/@href')
-
-    print(linkstable)
-
-
+    feed = rfeed.Feed(
+        title="Upmoon Filtered Manga Updates Feed",
+        link="https://rss.beanthemoonman.io:8443/manga/rss.xml",
+        description="Real degen hours",
+        language="en-US",
+        lastBuildDate=datetime.datetime.now(),
+        items=curateRssFeed(linksfeed,rssfeed)
+    )
+    print(feed.rss())
     
